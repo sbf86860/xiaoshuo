2f773095eb1e72920ad01dff84162bc51ff6a1f9
s2868 <s286877277@163.com>
2023-08-11 13:47:59 +0800

    1

--- down_text.py
+++ down_text.py
@@ -14,7 +14,6 @@ class down:
     def __init__(self):
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
-
         }
 
     @staticmethod
